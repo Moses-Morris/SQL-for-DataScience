1a53d687259cb92d9d4114a3dc050c0c04a8689a
Moses-Morris <mosesmorrisdev@gmail.com>
2023-03-03 16:41:05 +0300

    manipulation using pandas.

---
+++ read_panda_csv.py
@@ -0,0 +1,8 @@
+import pandas as pd
+openFile = pd.read_csv("athlete_events.csv")
+openFile.info() # prints the information and specifics of the dataset
+
+print(openFile['Name'])
+print(openFile.Name)
+openFile.Name.hist()
+print("Minimum Age is : {}".format(openFile.Age.min()))
