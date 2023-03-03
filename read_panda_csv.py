116e8ff949c4bf0fdaf80b4cd05da0cbebc89d4d
Moses-Morris <mosesmorrisdev@gmail.com>
2023-03-03 16:51:19 +0300

    Manipulation of data in pandas Project

--- read_panda_csv.py
+++ read_panda_csv.py
@@ -4,5 +4,8 @@ openFile.info() # prints the information and specifics of the dataset
 
 print(openFile['Name'])
 print(openFile.Name)
-openFile.Name.hist()
+#openFile.Name.hist()
 print("Minimum Age is : {}".format(openFile.Age.min()))
+openFile.info() # prints the information and specifics of the dataset
+openFile.Name.hist()
+openFile.head(28)
